064bc852dff22ee2f5fb5713284fbe29e2033472
saravit1 <teemu.saravirta@aalto.fi>
2019-11-03 23:01:58 +0200

    A tile pinned to the screen when scrolling over an element

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
-function App() {
+import ScrollMagic from 'scrollmagic'
+import Tile from './components/Tile'
+const App = () => {
+  const controller = new ScrollMagic.Controller()
+  
+  useEffect( () =>{
+    new ScrollMagic.Scene({ 
+      triggerElement: "#tilePin", 
+      duration: 1000, // scroll distance
+      offset: 200 // start this scene after scrolling for 50px
+     })
+      .setPin("#myElement") // pins the element for the the scene's duration
+      .addTo(controller) // assign the scene to the controller
+
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className='header'>
+        Scroll down to test pinning elements
       </header>
+      <div id='tilePin'/>
+      <div className='wrapper'>
+        <div id="myElement" className='tileWrapper'>
+          <Tile width='400px' height='400px' header='What to do today?' />
+        </div>
+      </div>
+      <div className='scroll'></div>
     </div>
-  );
+  )
 }
+  
 
 export default App;
